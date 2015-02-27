86bf4b98eac7f9b03cbee650ad95d13f7cda53e3
Vincent Batts <vbatts@hashbangbash.com>
2015-02-27 16:54:04 -0500

    tar/asm: more interface for (dis)assembly

--- tar/asm/getter.go
+++ tar/asm/getter.go
@@ -11,6 +11,16 @@ type FileGetter interface {
 	Get(string) (io.ReadCloser, error)
 }
 
+type FilePutter interface {
+	// Put returns a stream for the provided file path
+	Put(string, io.Writer) error
+}
+
+type FileGetPutter interface {
+	FileGetter
+	FilePutter
+}
+
 // NewPathFileGetter returns a FileGetter that is for files relative to path relpath.
 func NewPathFileGetter(relpath string) FileGetter {
 	return &pathFileGetter{root: relpath}
@@ -23,4 +33,23 @@ type pathFileGetter struct {
 func (pfg pathFileGetter) Get(filename string) (io.ReadCloser, error) {
 	// FIXME might should have a check for '../../../../etc/passwd' attempts?
 	return os.Open(path.Join(pfg.root, filename))
+}
+
+type bufferFileGetPutter struct {
+	files map[string][]byte
+}
+
+func (bfgp bufferFileGetPutter) Get(name string) (io.ReadCloser, error) {
+
+}
+
+type writeCloserWrapper struct {
+	io.Writer
+	closer func() error
+}
+
+func (w *nopWriteCloser) Close() error { return nil }
+
+func NewBufferFileGetPutter() FileGetPutter {
+	return &bufferFileGetPutter{}
 }
