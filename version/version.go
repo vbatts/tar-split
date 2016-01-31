b87f81631a2b1cb185737b5bea76a7e9e8c29723
Vincent Batts <vbatts@hashbangbash.com>
2016-01-31 01:39:10 -0500

    version: mark 0.9.12

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
 package version
 // AUTO-GENEREATED. DO NOT EDIT
-// 2015-08-14 09:56:50.742727493 -0400 EDT
+// 2016-01-31 01:39:06.012784413 -0500 EST
 
 // VERSION is the generated version from /home/vbatts/src/vb/tar-split/version
-var VERSION = "v0.9.6-1-gc76e420"
+var VERSION = "v0.9.12"
  
