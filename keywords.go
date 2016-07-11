f0f15a0e496c736fef3f182dba27ec30fedb56bf
Stephen Chung <schung@redhat.com>
2016-07-11 17:09:14 -0400

    keywords: return "link" keyword along with its value
    Originally only returning `Sys.linkname` when evaluating
    a tar header's link field. We want it to be in the form of
    keyword=value instead.
    
    Signed-off-by: Stephen Chung <schung@redhat.com>

--- keywords.go
+++ keywords.go
@@ -216,7 +216,10 @@ var (
 	}
 	linkKeywordFunc = func(path string, info os.FileInfo, r io.Reader) (string, error) {
 		if sys, ok := info.Sys().(*tar.Header); ok {
-			return sys.Linkname, nil
+			if sys.Linkname != "" {
+				return fmt.Sprintf("link=%s", sys.Linkname), nil
+			}
+			return "", nil
 		}
 
 		if info.Mode()&os.ModeSymlink != 0 {
