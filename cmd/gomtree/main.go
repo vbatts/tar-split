8cf725313285add1d64bea9bf4bfc52e1940ef0e
Aleksa Sarai <asarai@suse.de>
2016-07-21 08:08:24 +1000

    cmd: add --result-format
    This allows callers to deal with multiple output formats and not require
    string parsing in order to understand what the error was. The default
    format is "bsd".
    
    Signed-off-by: Aleksa Sarai <asarai@suse.de>

--- cmd/gomtree/main.go
+++ cmd/gomtree/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"log"
@@ -17,7 +18,19 @@ var (
 	flAddKeywords  = flag.String("K", "", "Add the specified (delimited by comma or space) keywords to the current set of keywords")
 	flUseKeywords  = flag.String("k", "", "Use the specified (delimited by comma or space) keywords as the current set of keywords")
 	flListKeywords = flag.Bool("list-keywords", false, "List the keywords available")
+	flResultFormat = flag.String("result-format", "bsd", "output the validation results using the given format (bsd, json, path)")
 )
+
+var formats = map[string]func(*mtree.Result) string{
+	// Outputs the errors in the BSD format.
+	"bsd": func(r *mtree.Result) string {
+		var buffer bytes.Buffer
+		for _, fail := range r.Failures {
+			fmt.Fprintln(&buffer, fail)
+		}
+		return buffer.String()
+	},
+}
 
 func main() {
 	flag.Parse()
@@ -40,6 +53,14 @@ func main() {
 				fmt.Println(" ", k)
 			}
 		}
+		return
+	}
+
+	// --output
+	formatFunc, ok := formats[*flResultFormat]
+	if !ok {
+		log.Printf("invalid output format: %s", *flResultFormat)
+		isErr = true
 		return
 	}
 
@@ -103,8 +124,11 @@ func main() {
 		}
 		if res != nil && len(res.Failures) > 0 {
 			defer os.Exit(1)
-			for _, failure := range res.Failures {
-				fmt.Println(failure)
+			out := formatFunc(res)
+			if _, err := os.Stdout.Write([]byte(out)); err != nil {
+				log.Println(err)
+				isErr = true
+				return
 			}
 		}
 	} else {
