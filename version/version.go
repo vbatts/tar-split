d3f1b54304d656376e58f9406a9cb4775799a357
Vincent Batts <vbatts@hashbangbash.com>
2016-09-26 19:53:52 -0400

    version: bump to v0.10.1
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

--- version/version.go
+++ version/version.go
@@ -1,7 +1,7 @@
 package version
 
 // AUTO-GENEREATED. DO NOT EDIT
-// 2016-09-23 11:00:18.92191222 -0400 EDT
+// 2016-09-26 19:53:30.825879 -0400 EDT
 
 // VERSION is the generated version from /home/vbatts/src/vb/tar-split/version
-var VERSION = "v0.10.0-9-gae8540d"
+var VERSION = "v0.10.1-4-gf280282"
