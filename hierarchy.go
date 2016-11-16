a8e4475c5ee41147b0416beae002ee1f48399d7f
Vincent Batts <vbatts@hashbangbash.com>
2016-11-16 14:15:42 -0500

    *: clean up
    * Get rid of the isErr func in main()
    * put main() logic closer to the top
    
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

--- hierarchy.go
+++ hierarchy.go
@@ -29,23 +29,24 @@ func (dh DirectoryHierarchy) WriteTo(w io.Writer) (n int64, err error) {
 // CollectUsedKeywords collects and returns all the keywords used in a
 // a DirectoryHierarchy
 func CollectUsedKeywords(dh *DirectoryHierarchy) []string {
-	if dh != nil {
-		usedkeywords := []string{}
-		for _, e := range dh.Entries {
-			switch e.Type {
-			case FullType, RelativeType, SpecialType:
-				if e.Type != SpecialType || e.Name == "/set" {
-					kvs := e.Keywords
-					for _, kv := range kvs {
-						kw := KeyVal(kv).Keyword()
-						if !inSlice(kw, usedkeywords) {
-							usedkeywords = append(usedkeywords, kw)
-						}
+	if dh == nil {
+		return nil
+	}
+
+	usedkeywords := []string{}
+	for _, e := range dh.Entries {
+		switch e.Type {
+		case FullType, RelativeType, SpecialType:
+			if e.Type != SpecialType || e.Name == "/set" {
+				kvs := e.Keywords
+				for _, kv := range kvs {
+					kw := KeyVal(kv).Keyword()
+					if !inSlice(kw, usedkeywords) {
+						usedkeywords = append(usedkeywords, kw)
 					}
 				}
 			}
 		}
-		return usedkeywords
 	}
-	return nil
+	return usedkeywords
 }
